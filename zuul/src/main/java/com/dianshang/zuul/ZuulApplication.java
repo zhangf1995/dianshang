96df21f8e5d40543d3745fa346d7fc4d0d32dec0
zf <820674670@qq.com>
2019-11-12 18:57:51 +0800

    zuul提交

---
+++ zuul/src/main/java/com/dianshang/zuul/ZuulApplication.java
@@ -0,0 +1,23 @@
+package com.dianshang.zuul;
+
+import com.dianshang.zuul.filter.TokenFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    @Bean
+    public TokenFilter tokenFilter(){
+        return new TokenFilter();
+    }
+}
